e6d6c30f49d17c4809731522624c8b07be1cae59
xdorro <xdorro@gmail.com>
2021-02-16 23:28:55 +0700

    Cập nhật VuePress blog theme

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,96 @@
+module.exports = {
+  title: 'Mediumish',
+  base: '/',
+  description: 'Blog example for Mediumish theme with VuePress',
+  logo: './assets/img/logo.png',
+  theme: require.resolve('../../'),
+  themeConfig: {
+  authors: [
+      {
+      name: 'Sal',
+      avatar: '/assets/img/sal.jpg',
+      link: 'https://wowthemes.net/donate',
+      linktext: 'Follow',
+      },
+      {
+        name: 'John Doe',
+        avatar: '/assets/img/avatar.png',
+        link: 'https://bootstrapstarter.com/',
+        linktext: 'Follow',
+      },
+    ],
+    footer: {
+      contact: [
+        {
+          type: 'codepen',
+          link: '#',
+        },
+        {
+          type: 'facebook',
+          link: '#',
+        },
+        {
+          type: 'github',
+          link: 'https://github.com/wowthemesnet/mediumish-vuepress-blog-theme',
+        },
+        {
+          type: 'gitlab',
+          link: '#',
+        },
+        {
+          type: 'instagram',
+          link: '#',
+        },
+        {
+          type: 'linkedin',
+          link: '#',
+        },
+        {
+          type: 'mail',
+          link: '#',
+        },
+        {
+          type: 'messenger',
+          link: '#',
+        },
+        {
+          type: 'phone',
+          link: '#',
+        },
+        {
+          type: 'twitter',
+          link: 'https://twitter.com/wowthemesnet',
+        },
+        {
+          type: 'web',
+          link: '#',
+        }
+      ],
+      copyright: [
+        {
+          text: 'Licensed MIT.',
+          link: 'https://bootstrapstarter.com/license/',
+        },
+        {
+          text: 'Made with Mediumish - free Vuepress theme',
+          link: 'https://bootstrapstarter.com/bootstrap-templates/vuepress-theme-mediumish/',
+        },
+      ],
+    },
+
+    sitemap: {
+      hostname: 'https://github.com/xdorro/vuepress-xdorro-blog/'
+    },
+    comment: {
+      service: 'disqus',
+      shortname: 'demowebsite',
+    },
+    newsletter: {
+      endpoint: 'https://wowthemes.us11.list-manage.com/subscribe/post?u=8aeb20a530e124561927d3bd8&id=8c3d2d214b'
+    },
+    feed: {
+      canonical_base: 'https://github.com/xdorro/vuepress-xdorro-blog/',
+    },
+    smoothScroll: true
+  },
+}
